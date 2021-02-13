e6084dbccc26fa1c70df3914e6dec2f4eb86fad2
Dang Chuong Pham <dangchuong1999@outlook.com>
2021-02-13 21:08:20 +0200

    add-todo component. Create form, use ngModel and ngSubmit directives to submit data.

---
+++ src/app/components/add-todo/add-todo.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.component.html',
+  styleUrls: ['./add-todo.component.css']
+})
+export class AddTodoComponent implements OnInit {
+  title: string = ""
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log('[Component add-todo] submitted', this.title)
+  }
+
+}
